9569a1457845d0e1004355aec30601b1e8d31ba1
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-03 15:28:47 +0100

    Update core.h

--- src/core.h
+++ src/core.h
@@ -1,9 +1,13 @@
+#ifndef CORE_H
+#define CORE_H
+
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint64_t U64;
+void init_board();
 
 // Bitboards for all pieces.
 U64 white_pawns, white_rooks, white_knights, white_bishops, white_queen, white_king;
 U64 black_pawns, black_rooks, black_knights, black_bishops, black_queen, black_king;
 
+#endif
