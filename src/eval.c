1e439d7489069d52a64df433e31d409bd184fc55
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-03 15:34:12 +0100

    Added all piece values. Its still super early development.

---
+++ src/eval.c
@@ -0,0 +1,18 @@
+#ifned EVAL_C
+#define EVAL_C
+
+int evalBoard() {
+  int score = 0;
+
+  int pawn_value = 100; 
+  int rook_value = 500; 
+  int knight_value = 320; 
+  int bishop_value = 330;
+  int queen_value = 900;
+  int king_value = 20000; 
+
+   
+}
+
+
+#endif
