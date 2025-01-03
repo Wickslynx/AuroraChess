ca0b4d74807302fe2e922f752e482b3069571410
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-03 15:19:54 +0100

    Create board.h

---
+++ src/board.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint64_t U64;
+
+// Bitboards for all pieces.
+U64 white_pawns, white_rooks, white_knights, white_bishops, white_queen, white_king;
+U64 black_pawns, black_rooks, black_knights, black_bishops, black_queen, black_king;
+
